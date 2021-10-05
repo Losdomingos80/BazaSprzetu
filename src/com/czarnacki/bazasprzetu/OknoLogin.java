d20d3110ebde63bc1b1fc709e54357b978cbd6e0
Losdomingos80 <dominik@zparczewa.pl>
2021-10-05 13:31:46 +0200

    'OkienkoLogowania'

--- src/com/czarnacki/bazasprzetu/OknoLogin.java
+++ src/com/czarnacki/bazasprzetu/OknoLogin.java
@@ -6,6 +6,13 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.Color;
 
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
@@ -14,12 +21,12 @@ import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 
 @SuppressWarnings("serial")
-public class OknoLogin extends JFrame {
+public class OknoLogin extends JFrame implements ActionListener, KeyListener, FocusListener{
 
 	private JPanel contentPane;
-	private JTextField textField;
+	private JTextField inputLogin;
 	private JLabel textHaslo;
-	private JPasswordField passwordField;
+	private JPasswordField inputHaslo;
 	private JSeparator separator;
 	private JButton buttonLogowanie;
 
@@ -36,32 +43,118 @@ public class OknoLogin extends JFrame {
 				(Toolkit.getDefaultToolkit().getScreenSize().height - getSize().height) / 2);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(0, 2, 6, 2));
+		setContentPane(contentPane);
 		
 		JLabel textLogin = new JLabel("Login", SwingConstants.RIGHT);
 		textLogin.setVerticalAlignment(SwingConstants.TOP);
 		contentPane.add(textLogin);
 		
-		textField = new JTextField();
-		contentPane.add(textField);
-		textField.setColumns(15);
+		inputLogin = new JTextField();
+		inputLogin.addKeyListener(this);
+		inputLogin.addFocusListener(this);
+		inputLogin.setColumns(15);
+		contentPane.add(inputLogin);
+		
 		
 		textHaslo = new JLabel("Has\u0142o", SwingConstants.RIGHT);
 		textHaslo.setVerticalAlignment(SwingConstants.TOP);
 		contentPane.add(textHaslo);
 		
-		passwordField = new JPasswordField();
-		passwordField.setColumns(15);
-		contentPane.add(passwordField);
+		inputHaslo = new JPasswordField();
+		inputHaslo.setColumns(15);
+		inputHaslo.addKeyListener(this);
+		inputHaslo.addFocusListener(this);
+		contentPane.add(inputHaslo);
 		
 		separator = new JSeparator();
 		contentPane.add(separator);
 		
 		buttonLogowanie = new JButton("Logowanie");
+		buttonLogowanie.addActionListener(this);
+		buttonLogowanie.addKeyListener(this);
 		contentPane.add(buttonLogowanie);
 		pack();
 		setVisible(true);
 	}
+
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		Object source = e.getSource();
+		if (e.getKeyCode()==KeyEvent.VK_ENTER){
+			if(source == buttonLogowanie) {
+				try {
+					System.out.println("logowanie");
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			if(source == inputLogin) {
+				inputHaslo.requestFocus();
+			}
+			if(source == inputHaslo) {
+				buttonLogowanie.requestFocus();
+			}
+			
+			
+        }
+	}
+
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		Object source = e.getSource();
+		 
+ 		if(source == buttonLogowanie) {}
+ 			try {
+ 				System.out.println("logowanie");
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		
+	}
+	
+	public void focusGained(FocusEvent e) {
+		Object source = e.getSource();
+		if(source == inputLogin) {
+			inputLogin.setBackground(Color.CYAN);
+		}
+		if(source == inputHaslo) {
+			inputHaslo.setBackground(Color.CYAN);
+		}
+    }
+
+    public void focusLost(FocusEvent e) {
+    	Object source = e.getSource();
+		if(source == inputLogin) {
+			inputLogin.setBackground(Color.WHITE);
+		}
+		if(source == inputHaslo) {
+			inputHaslo.setBackground(Color.WHITE);
+		}
+    }
 
 }
